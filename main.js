b5c40ffd8dde874f8e91e4fd13bafb3158a13552
Victoria Pittard <38929314+victoriamp@users.noreply.github.com>
2019-12-29 16:22:10 -0500

    added content layout, needs formatting still

--- main.js
+++ main.js
@@ -1,24 +1,50 @@
-import Typed from 'typed.js';
+(function($){
+	"use strict";
 
-var options = {
-  strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
-  typeSpeed: 40
-};
-
-var typed = new Typed('.element', options);
-
-$("document").ready(function(){
-	handleHome();
-})
-
-function handleHome(){
-	$(".element").typed({
-		strings: [test, test1, test2],
-		typeSpeed: 50,
-		starDelay: 200,
-		backDelay: 600,
-		loop: true,
-		showCursor: true,
-		cursorChar: "|"
+	//scroll smoothly
+	$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function(){
+		if(location.pathname.replace(/^\//, '')==this.pathname.replace(/^\//, '') && location.hostname==this.hostname){
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if(target.length){
+				$('html, body').animate({
+					scrollTop: (target.offset().top-71)
+				}, 1000, "easeInOutExpo");
+				return false;
+			}
+		}
 	});
-}
+
+	//scroll-to-top button 
+	$(document).scroll(function(){
+		var scrollDistance = $(this).scrollTop();
+		if(scrollDistance>100){
+			$('.scroll-to-top').fadeIn();
+		}else{
+			$('.scroll-to-top').fadeOut();
+		}
+	});
+
+	//responsive navigation menu
+	$('.js-scroll-trigger').click(function(){
+		$('.navbar-collapse').collapse('hide');
+	});
+
+	//active/inactive navbar items
+	$('body').scrollspy({
+		target: '#mainNav', 
+		offset: 80
+	})
+
+	//collapse navbar
+	var navbarCollapse = function(){
+		if($("#mainNav").offset().top>100){
+			$("#mainNav").addClass("navbar-shrink");
+		}else{
+			$("#mainNav").removeClass("navbar-shrink");
+		}
+	}
+	navbarCollapse();
+	$(window).scroll(navbarCollapse);
+
+})(jQuery);
